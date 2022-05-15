44ef896f042b3eea2066aa7c20563ca2567c8ed1
andreea <grigoreandreea18@stud.ase.ro>
2022-05-15 18:37:13 +0300

    last version running

---
+++ src/javascript/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    const {title}= props;
+    return ( 
+        <header className='h-14 bg-blue-700 flex justify-center'>
+            <span className='self-center text-white text-bold text-xl'>{title}</span>
+        </header>
+     );
+}
+
+export default Header;
